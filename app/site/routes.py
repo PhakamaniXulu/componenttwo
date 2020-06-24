dcc7b3f0893f53ca4d0293097594ffd828b4c3f9
PhakamaniXulu <phakamani@black-box.io>
2020-06-24 12:21:15 +0200

    Html Structure

--- app/site/routes.py
+++ app/site/routes.py
@@ -1,8 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 mod = Blueprint('site',__name__)
+
+
 
 @mod.route('/')
 @mod.route('/index')
 def index():
-    return "test"
+    return render_template('index.html', title='Home')
+
+@mod.route('/financial')
+def financial():
+    return render_template('financial.html', title='Financial')
