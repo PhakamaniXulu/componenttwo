5c3e8da0484a97bac9ad574bde3a04d70a01a2d3
PhakamaniXulu <phakamani@black-box.io>
2020-06-28 03:08:49 +0200

    Accordion and Nav Pills

--- app/static/src/js/scripts.js
+++ app/static/src/js/scripts.js
@@ -1,50 +1,7 @@
-$(document).ready(function(){
-
-    var panel = $('.panel').css('width');
-    var width = panel.replace('px','');
-    var i=0;
-    $('ul li').each(function(){
-        i++;
+$(document).ready(function() {
+    $(".nav .nav-item").click(function () {
+        $(".nav .nav-item").removeClass("btn-primary").addClass("btn-default");
+        // $(".tab").addClass("active"); // instead of this do the below 
+        $(this).removeClass("btn-default").addClass("btn-primary");   
     });
-    var val1 = width/i + 5
-
-    $('.panel').css('width',val1 + 'px');
-
-    //----------
-        $('ul li:nth-child(1)').on('click',function(){
-        $('.panel').animate({
-            left: '0px'
-        })
     });
-    $('ul li:nth-child(2)').on('click',function(){
-        $('.panel').animate({
-            left: val1 + 20+ 'px'
-        })
-    });
-    $('ul li:nth-child(3)').on('click',function(){
-        $('.panel').animate({
-            left: val1 + val1 + 30+ 'px'
-        })
-    });
-
-    $('ul li:nth-child(4)').on('click',function(){
-        $('.panel').animate({
-            left: val1 + val1 + val1 + 40 + 'px'
-        })
-    });
-
-    $('ul li:nth-child(5)').on('click',function(){
-        $('.panel').animate({
-            left: val1 + val1 + val1 + val1 + 50 + 'px'
-        })
-    });
-
-    $('ul li:nth-child(6)').on('click',function(){
-        $('.panel').animate({
-            left: val1 +val1 + val1 + val1 + val1 + 60 + 'px'
-        })
-    });
-
-
-
-});
